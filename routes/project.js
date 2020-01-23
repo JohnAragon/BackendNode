7dfa6aebee6d5fcec4756b8cdf9db3f5a823bf3f
John Aragon <j-aragon@hotmail.com>
2020-01-23 09:36:03 -0500

    Version inicial BackendNode

---
+++ routes/project.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var express = require('express');
+var ProjectController = require('../controllers/project');
+
+var router = express.Router();
+
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart({uploadDir:'./uploads'}); 
+
+router.get('/home',ProjectController.home);
+router.post('/test',ProjectController.test);
+router.post('/project/save',ProjectController.saveProject);
+router.post('/project/:id?',ProjectController.getProject);
+router.get('/projects',ProjectController.getProjects);
+router.put('/project/update/:id',ProjectController.updateProject);
+router.post('/project/update/upload-image/:id',multipartMiddleware, ProjectController.uploadImage);
+router.delete('/project/delete/:id',ProjectController.deleteProject);
+
+
+module.exports = router;
